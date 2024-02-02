34181b39a9c34358a3423484a8cfe3f5b5e41e3e
Nathan E. Egge <negge@xiph.org>
2024-02-02 05:53:48 -0500

    Add the world's simplest C memcpy.

---
+++ src/memcpy.c
@@ -0,0 +1,7 @@
+#include "memcpy.h"
+
+void *memcpy(void *dest, const void *src, size_t n) {
+  char *cdest = (char *)dest;
+  const char *csrc = (const char *)src;
+  while (n-- > 0) *cdest++ = *csrc++;
+}
