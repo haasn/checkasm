5fb9b825d85e34f44eb56486311209543c570498
Martin Storsjö <martin@martin.st>
2024-11-20 13:57:18 +0200

    checkasm: looprestoration: Do strict bounds checking of the output
    Extend the PIXEL_RECT macro to provide a variable containing the
    full, padded height of the buffer, for uses that operate on the
    full buffer.

--- include/checkasm/test.h
+++ include/checkasm/test.h
@@ -175,6 +175,8 @@ CHECKASM_API void checkasm_checked_call(void *func, ...);
     ALIGN_STK_64(pixel, name##_buf, ((h)+32)*(ROUND_UP(w,64)+64) + 64,); \
     ptrdiff_t name##_stride = sizeof(pixel)*(ROUND_UP(w,64)+64); \
     (void)name##_stride; \
+    int name##_buf_h = (h)+32; \
+    (void)name##_buf_h;\
     pixel *name = name##_buf + (ROUND_UP(w,64)+64)*16 + 64
 
 #define CLEAR_PIXEL_RECT(name) \
