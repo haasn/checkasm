da4adc519d8e5c234dc2d09ff137182d2283c64f
Niklas Haas <git@haasn.dev>
2025-10-01 18:52:11 +0200

    checkasm.h: clean up, format, whitespace
    Also remove an unused/unneeded config macro.

--- checkasm/checkasm.h
+++ checkasm/checkasm.h
@@ -30,6 +30,12 @@
 
 #include <stdint.h>
 
+#include "common/attributes.h"
+
+/*************************************
+ * Per-project configuration structs *
+ ************************************/
+
 typedef struct CheckasmCpuFlag {
     const char *name;
     const char *suffix;
@@ -100,40 +106,38 @@ int checkasm_run(const CheckasmConfig *config);
  ********************************************/
 
 #ifdef _WIN32
-#include <windows.h>
-#if ARCH_X86_32
-#include <setjmp.h>
-typedef jmp_buf checkasm_context;
-#define checkasm_save_context() setjmp(checkasm_context_buf)
-#define checkasm_load_context() longjmp(checkasm_context_buf, 1)
-#elif WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
-/* setjmp/longjmp on Windows on architectures using SEH (all except x86_32)
- * will try to use SEH to unwind the stack, which doesn't work for assembly
- * functions without unwind information. */
-typedef struct { CONTEXT c; int status; } checkasm_context;
-#define checkasm_save_context() \
-    (checkasm_context_buf.status = 0, \
-     RtlCaptureContext(&checkasm_context_buf.c), \
-     checkasm_context_buf.status)
-#define checkasm_load_context() \
-    (checkasm_context_buf.status = 1, \
-     RtlRestoreContext(&checkasm_context_buf.c, NULL))
-#else
-typedef void* checkasm_context;
-#define checkasm_save_context() 0
-#define checkasm_load_context() do {} while (0)
-#endif
+    #include <windows.h>
+    #if ARCH_X86_32
+        #include <setjmp.h>
+        typedef jmp_buf checkasm_context;
+        #define checkasm_save_context() setjmp(checkasm_context_buf)
+        #define checkasm_load_context() longjmp(checkasm_context_buf, 1)
+    #elif WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+        /* setjmp/longjmp on Windows on architectures using SEH (all except
+         * x86_32) will try to use SEH to unwind the stack, which doesn't work
+         * for assembly functions without unwind information. */
+        typedef struct { CONTEXT c; int status; } checkasm_context;
+        #define checkasm_save_context() \
+            (checkasm_context_buf.status = 0, \
+            RtlCaptureContext(&checkasm_context_buf.c), \
+            checkasm_context_buf.status)
+        #define checkasm_load_context() \
+            (checkasm_context_buf.status = 1, \
+            RtlRestoreContext(&checkasm_context_buf.c, NULL))
+    #else
+        typedef void* checkasm_context;
+        #define checkasm_save_context() 0
+        #define checkasm_load_context() do {} while (0)
+    #endif
 #else /* !_WIN32 */
-#include <setjmp.h>
-typedef sigjmp_buf checkasm_context;
-#define checkasm_save_context() sigsetjmp(checkasm_context_buf, 1)
-#define checkasm_load_context() siglongjmp(checkasm_context_buf, 1)
+    #include <setjmp.h>
+    typedef sigjmp_buf checkasm_context;
+    #define checkasm_save_context() sigsetjmp(checkasm_context_buf, 1)
+    #define checkasm_load_context() siglongjmp(checkasm_context_buf, 1)
 #endif
-
-#include "common/attributes.h"
 
 #if ARCH_ARM
-#include "src/arm/arm-arch.h"
+    #include "src/arm/arm-arch.h"
 #endif
 
 int xor128_rand(void);
@@ -189,102 +193,101 @@ int double_near_abs_eps_array(const double *a, const double *b, double eps,
      ((func_type *)func_ref)(__VA_ARGS__));\
     checkasm_set_signal_handler_state(0)
 
-#if HAVE_ASM
 #if ARCH_X86
-#if defined(_MSC_VER) && !defined(__clang__)
-#include <intrin.h>
-#define readtime() (_mm_lfence(), __rdtsc())
-#else
-static inline uint64_t readtime(void) {
-    uint32_t eax, edx;
-    __asm__ __volatile__("lfence\nrdtsc" : "=a"(eax), "=d"(edx));
-    return (((uint64_t)edx) << 32) | eax;
-}
-#define readtime readtime
-#endif
+    #if defined(_MSC_VER) && !defined(__clang__)
+        #include <intrin.h>
+        #define readtime() (_mm_lfence(), __rdtsc())
+    #else
+        static inline uint64_t readtime(void) {
+            uint32_t eax, edx;
+            __asm__ __volatile__("lfence\nrdtsc" : "=a"(eax), "=d"(edx));
+            return (((uint64_t)edx) << 32) | eax;
+        }
+        #define readtime readtime
+    #endif
 #elif CONFIG_MACOS_KPERF
-uint64_t checkasm_kperf_cycles(void);
-#define readtime() checkasm_kperf_cycles()
+    uint64_t checkasm_kperf_cycles(void);
+    #define readtime() checkasm_kperf_cycles()
 #elif (ARCH_AARCH64 || ARCH_ARM) && defined(__APPLE__)
-#include <mach/mach_time.h>
-#define readtime() mach_absolute_time()
+    #include <mach/mach_time.h>
+    #define readtime() mach_absolute_time()
 #elif ARCH_AARCH64
-#ifdef _MSC_VER
-#include <windows.h>
-#define readtime() (_InstructionSynchronizationBarrier(), ReadTimeStampCounter())
-#else
-static inline uint64_t readtime(void) {
-    uint64_t cycle_counter;
-    /* This requires enabling user mode access to the cycle counter (which
-     * can only be done from kernel space).
-     * This could also read cntvct_el0 instead of pmccntr_el0; that register
-     * might also be readable (depending on kernel version), but it has much
-     * worse precision (it's a fixed 50 MHz timer). */
-    __asm__ __volatile__("isb\nmrs %0, pmccntr_el0"
-                         : "=r"(cycle_counter)
-                         :: "memory");
-    return cycle_counter;
-}
-#define readtime readtime
-#endif
+    #ifdef _MSC_VER
+        #include <windows.h>
+        #define readtime() (_InstructionSynchronizationBarrier(), ReadTimeStampCounter())
+    #else
+        static inline uint64_t readtime(void) {
+            uint64_t cycle_counter;
+            /* This requires enabling user mode access to the cycle counter
+             * (which can only be done from kernel space).
+             * This could also read cntvct_el0 instead of pmccntr_el0; that register
+             * might also be readable (depending on kernel version), but it has much
+             * worse precision (it's a fixed 50 MHz timer). */
+            __asm__ __volatile__("isb\nmrs %0, pmccntr_el0"
+                                : "=r"(cycle_counter)
+                                :: "memory");
+            return cycle_counter;
+        }
+        #define readtime readtime
+    #endif
 #elif ARCH_ARM && !defined(_MSC_VER) && __ARM_ARCH >= 7
-static inline uint64_t readtime(void) {
-    uint32_t cycle_counter;
-    /* This requires enabling user mode access to the cycle counter (which
-     * can only be done from kernel space). */
-    __asm__ __volatile__("isb\nmrc p15, 0, %0, c9, c13, 0"
-                         : "=r"(cycle_counter)
-                         :: "memory");
-    return cycle_counter;
-}
-#define readtime readtime
+    static inline uint64_t readtime(void) {
+        uint32_t cycle_counter;
+        /* This requires enabling user mode access to the cycle counter (which
+         * can only be done from kernel space). */
+        __asm__ __volatile__("isb\nmrc p15, 0, %0, c9, c13, 0"
+                            : "=r"(cycle_counter)
+                            :: "memory");
+        return cycle_counter;
+    }
+    #define readtime readtime
 #elif ARCH_PPC64LE
-static inline uint64_t readtime(void) {
-    uint32_t tbu, tbl, temp;
+    static inline uint64_t readtime(void) {
+        uint32_t tbu, tbl, temp;
 
-    __asm__ __volatile__(
-        "1:\n"
-        "mfspr %2,269\n"
-        "mfspr %0,268\n"
-        "mfspr %1,269\n"
-        "cmpw   %2,%1\n"
-        "bne    1b\n"
-    : "=r"(tbl), "=r"(tbu), "=r"(temp)
-    :
-    : "cc");
+        __asm__ __volatile__(
+            "1:\n"
+            "mfspr %2,269\n"
+            "mfspr %0,268\n"
+            "mfspr %1,269\n"
+            "cmpw   %2,%1\n"
+            "bne    1b\n"
+        : "=r"(tbl), "=r"(tbu), "=r"(temp)
+        :
+        : "cc");
 
-    return (((uint64_t)tbu) << 32) | (uint64_t)tbl;
-}
-#define readtime readtime
+        return (((uint64_t)tbu) << 32) | (uint64_t)tbl;
+    }
+    #define readtime readtime
 #elif ARCH_RISCV
-#include <time.h>
-static inline uint64_t clock_gettime_nsec(void) {
-  struct timespec ts;
-#ifdef CLOCK_MONOTONIC_RAW
-  clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
-#else
-  clock_gettime(CLOCK_MONOTONIC, &ts);
-#endif
-  return ((uint64_t)ts.tv_sec*1000000000u) + (uint64_t)ts.tv_nsec;
-}
-#define readtime clock_gettime_nsec
+    #include <time.h>
+    static inline uint64_t clock_gettime_nsec(void) {
+        struct timespec ts;
+    #ifdef CLOCK_MONOTONIC_RAW
+        clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+    #else
+        clock_gettime(CLOCK_MONOTONIC, &ts);
+    #endif
+        return ((uint64_t)ts.tv_sec*1000000000u) + (uint64_t)ts.tv_nsec;
+    }
+    #define readtime clock_gettime_nsec
 #elif ARCH_LOONGARCH
-static inline uint64_t readtime(void) {
-#if ARCH_LOONGARCH64
-    uint64_t a, id;
-    __asm__ __volatile__("rdtime.d  %0, %1"
-                         : "=r"(a), "=r"(id)
-                         :: );
-    return a;
-#else
-    uint32_t a, id;
-    __asm__ __volatile__("rdtimel.w  %0, %1"
-                         : "=r"(a), "=r"(id)
-                         :: );
-    return (uint64_t)a;
-#endif
-}
-#define readtime readtime
+    static inline uint64_t readtime(void) {
+    #if ARCH_LOONGARCH64
+        uint64_t a, id;
+        __asm__ __volatile__("rdtime.d  %0, %1"
+                            : "=r"(a), "=r"(id)
+                            :: );
+        return a;
+    #else
+        uint32_t a, id;
+        __asm__ __volatile__("rdtimel.w  %0, %1"
+                            : "=r"(a), "=r"(id)
+                            :: );
+      return (uint64_t)a;
+    #endif
+    }
+    #define readtime readtime
 #endif
 
 /* Verifies that clobbered callee-saved registers
@@ -292,6 +295,7 @@ static inline uint64_t readtime(void) {
 void checkasm_checked_call(void *func, ...);
 
 #if ARCH_X86_64
+
 /* YMM and ZMM registers on x86 are turned off to save power when they haven't
  * been used for some period of time. When they are used there will be a
  * "warmup" period during which performance will be reduced and inconsistent
@@ -308,12 +312,13 @@ void checkasm_simd_warmup(void);
  * specifying which parameters should have their upper halves clobbered. */
 #ifdef _WIN32
 /* Integer and floating-point parameters share "register slots". */
-#define IGNORED_FP_ARGS 0
+  #define IGNORED_FP_ARGS 0
 #else
 /* Up to 8 floating-point parameters are passed in XMM registers, which are
  * handled orthogonally from integer parameters passed in GPR registers. */
-#define IGNORED_FP_ARGS 8
+  #define IGNORED_FP_ARGS 8
 #endif
+
 #if HAVE_C11_GENERIC
 #define clobber_type(arg) _Generic((void (*)(void*, arg))NULL,\
      void (*)(void*, int32_t ): clobber_mask |= 1 << mpos++,\
@@ -331,9 +336,10 @@ void checkasm_simd_warmup(void);
         clobber_type(m); clobber_type(n); clobber_type(o); clobber_type(p);\
     }
 #else
-/* Skip parameter clobbering on compilers without support for _Generic() */
-#define init_clobber_mask(...) unsigned clobber_mask = 0
+  /* Skip parameter clobbering on compilers without support for _Generic() */
+  #define init_clobber_mask(...) unsigned clobber_mask = 0
 #endif
+
 #define declare_new(ret, ...)\
     ret (*checked_call)(__VA_ARGS__, int, int, int, int, int, int, int,\
                         int, int, int, int, int, int, int, int, int,\
@@ -348,84 +354,77 @@ void checkasm_simd_warmup(void);
      checked_call(__VA_ARGS__, 16, 15, 14, 13, 12, 11, 10, 9, 8,\
                   7, 6, 5, 4, 3, 2, 1, func_new, clobber_mask));\
     checkasm_set_signal_handler_state(0)
+
 #elif ARCH_X86_32
-#define declare_new(ret, ...)\
-    ret (*checked_call)(void *, __VA_ARGS__, int, int, int, int, int, int,\
-                        int, int, int, int, int, int, int, int, int) =\
-        (void *)checkasm_checked_call;
-#define call_new(...)\
-    (checkasm_set_signal_handler_state(1),\
-     checked_call(func_new, __VA_ARGS__, 15, 14, 13, 12,\
-                  11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1));\
-    checkasm_set_signal_handler_state(0)
+    #define declare_new(ret, ...)\
+        ret (*checked_call)(void *, __VA_ARGS__, int, int, int, int, int, int,\
+                            int, int, int, int, int, int, int, int, int) =\
+            (void *)checkasm_checked_call;
+    #define call_new(...)\
+        (checkasm_set_signal_handler_state(1),\
+        checked_call(func_new, __VA_ARGS__, 15, 14, 13, 12,\
+                    11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1));\
+        checkasm_set_signal_handler_state(0)
 #elif ARCH_ARM
-/* Use a dummy argument, to offset the real parameters by 2, not only 1.
- * This makes sure that potential 8-byte-alignment of parameters is kept
- * the same even when the extra parameters have been removed. */
-extern void (*checkasm_checked_call_ptr)(void *func, int dummy, ...);
-#define declare_new(ret, ...)\
-    ret (*checked_call)(void *, int dummy, __VA_ARGS__,\
-                        int, int, int, int, int, int, int, int,\
-                        int, int, int, int, int, int, int) =\
-    (void *)checkasm_checked_call_ptr;
-#define call_new(...)\
-    (checkasm_set_signal_handler_state(1),\
-     checked_call(func_new, 0, __VA_ARGS__, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0, 0));\
-    checkasm_set_signal_handler_state(0)
+    /* Use a dummy argument, to offset the real parameters by 2, not only 1.
+    * This makes sure that potential 8-byte-alignment of parameters is kept
+    * the same even when the extra parameters have been removed. */
+    extern void (*checkasm_checked_call_ptr)(void *func, int dummy, ...);
+    #define declare_new(ret, ...)\
+        ret (*checked_call)(void *, int dummy, __VA_ARGS__,\
+                            int, int, int, int, int, int, int, int,\
+                            int, int, int, int, int, int, int) =\
+        (void *)checkasm_checked_call_ptr;
+    #define call_new(...)\
+        (checkasm_set_signal_handler_state(1),\
+        checked_call(func_new, 0, __VA_ARGS__, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0, 0));\
+        checkasm_set_signal_handler_state(0)
 #elif ARCH_AARCH64 && !defined(__APPLE__)
-void checkasm_stack_clobber(uint64_t clobber, ...);
-#define declare_new(ret, ...)\
-    ret (*checked_call)(void *, int, int, int, int, int, int, int,\
-                        __VA_ARGS__, int, int, int, int, int, int, int, int,\
-                        int, int, int, int, int, int, int) =\
-    (void *)checkasm_checked_call;
-#define CLOB (UINT64_C(0xdeadbeefdeadbeef))
-#define call_new(...)\
-    (checkasm_set_signal_handler_state(1),\
-     checkasm_stack_clobber(CLOB, CLOB, CLOB, CLOB, CLOB, CLOB,\
-                            CLOB, CLOB, CLOB, CLOB, CLOB, CLOB,\
-                            CLOB, CLOB, CLOB, CLOB, CLOB, CLOB,\
-                            CLOB, CLOB, CLOB, CLOB, CLOB),\
-     checked_call(func_new, 0, 0, 0, 0, 0, 0, 0, __VA_ARGS__,\
-                  7, 6, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0));\
-    checkasm_set_signal_handler_state(0)
+    void checkasm_stack_clobber(uint64_t clobber, ...);
+    #define declare_new(ret, ...)\
+        ret (*checked_call)(void *, int, int, int, int, int, int, int,\
+                            __VA_ARGS__, int, int, int, int, int, int, int, int,\
+                            int, int, int, int, int, int, int) =\
+        (void *)checkasm_checked_call;
+    #define CLOB (UINT64_C(0xdeadbeefdeadbeef))
+    #define call_new(...)\
+        (checkasm_set_signal_handler_state(1),\
+        checkasm_stack_clobber(CLOB, CLOB, CLOB, CLOB, CLOB, CLOB,\
+                                CLOB, CLOB, CLOB, CLOB, CLOB, CLOB,\
+                                CLOB, CLOB, CLOB, CLOB, CLOB, CLOB,\
+                                CLOB, CLOB, CLOB, CLOB, CLOB),\
+        checked_call(func_new, 0, 0, 0, 0, 0, 0, 0, __VA_ARGS__,\
+                    7, 6, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0));\
+        checkasm_set_signal_handler_state(0)
 #elif ARCH_RISCV
-#define declare_new(ret, ...)\
-    ret (*checked_call)(void *, int, int, int, int, int, int, int,\
-                        __VA_ARGS__, int, int, int, int, int, int, int, int,\
-                        int, int, int, int, int, int, int) =\
-    (void *)checkasm_checked_call;
-#define call_new(...)\
-    (checkasm_set_signal_handler_state(1),\
-     checked_call(func_new, 0, 0, 0, 0, 0, 0, 0, __VA_ARGS__,\
-                  7, 6, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0));\
-    checkasm_set_signal_handler_state(0)
+    #define declare_new(ret, ...)\
+        ret (*checked_call)(void *, int, int, int, int, int, int, int,\
+                            __VA_ARGS__, int, int, int, int, int, int, int, int,\
+                            int, int, int, int, int, int, int) =\
+        (void *)checkasm_checked_call;
+    #define call_new(...)\
+        (checkasm_set_signal_handler_state(1),\
+        checked_call(func_new, 0, 0, 0, 0, 0, 0, 0, __VA_ARGS__,\
+                    7, 6, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0));\
+        checkasm_set_signal_handler_state(0)
 #elif ARCH_LOONGARCH
-#define declare_new(ret, ...)\
-    ret (*checked_call)(void *, int, int, int, int, int, int, int,\
-                        __VA_ARGS__, int, int, int, int, int, int, int, int,\
-                        int, int, int, int, int, int, int) =\
-    (void *)checkasm_checked_call;
-#define call_new(...)\
-    (checkasm_set_signal_handler_state(1),\
-     checked_call(func_new, 0, 0, 0, 0, 0, 0, 0, __VA_ARGS__,\
-                  7, 6, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0));\
-    checkasm_set_signal_handler_state(0)
+    #define declare_new(ret, ...)\
+        ret (*checked_call)(void *, int, int, int, int, int, int, int,\
+                            __VA_ARGS__, int, int, int, int, int, int, int, int,\
+                            int, int, int, int, int, int, int) =\
+        (void *)checkasm_checked_call;
+    #define call_new(...)\
+        (checkasm_set_signal_handler_state(1),\
+        checked_call(func_new, 0, 0, 0, 0, 0, 0, 0, __VA_ARGS__,\
+                    7, 6, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0));\
+        checkasm_set_signal_handler_state(0)
 #else
-#define declare_new(ret, ...)
-#define call_new(...)\
-    (checkasm_set_signal_handler_state(1),\
-     ((func_type *)func_new)(__VA_ARGS__));\
-    checkasm_set_signal_handler_state(0)
+    #define declare_new(ret, ...)
+    #define call_new(...)\
+        (checkasm_set_signal_handler_state(1),\
+        ((func_type *)func_new)(__VA_ARGS__));\
+        checkasm_set_signal_handler_state(0)
 #endif
-#else /* HAVE_ASM */
-#define declare_new(ret, ...)
-/* Call the function */
-#define call_new(...)\
-    (checkasm_set_signal_handler_state(1),\
-     ((func_type *)func_new)(__VA_ARGS__));\
-    checkasm_set_signal_handler_state(0)
-#endif /* HAVE_ASM */
 
 #define CALL4(...)\
     do {\
@@ -525,10 +524,10 @@ int checkasm_check_float_ulp(const char *file, int line,
 #define checkasm_check(prefix, ...) checkasm_check2(prefix, __VA_ARGS__, 0, 0, 0)
 
 #ifdef BITDEPTH
-#define checkasm_check_pixel(...) checkasm_check(PIXEL_TYPE, __VA_ARGS__)
-#define checkasm_check_pixel_padded(...) checkasm_check2(PIXEL_TYPE, __VA_ARGS__, 1, 1, 8)
-#define checkasm_check_pixel_padded_align(...) checkasm_check2(PIXEL_TYPE, __VA_ARGS__, 8)
-#define checkasm_check_coef(...)  checkasm_check(COEF_TYPE,  __VA_ARGS__)
+    #define checkasm_check_pixel(...) checkasm_check(PIXEL_TYPE, __VA_ARGS__)
+    #define checkasm_check_pixel_padded(...) checkasm_check2(PIXEL_TYPE, __VA_ARGS__, 1, 1, 8)
+    #define checkasm_check_pixel_padded_align(...) checkasm_check2(PIXEL_TYPE, __VA_ARGS__, 8)
+    #define checkasm_check_coef(...)  checkasm_check(COEF_TYPE,  __VA_ARGS__)
 #endif
 
 #endif /* CHECKASM_CHECKASM_H */
