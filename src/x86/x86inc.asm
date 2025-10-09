3eccd9b36d4142e47eff28793b686b298706d23f
Niklas Haas <git@haasn.dev>
2025-10-09 20:00:06 +0200

    x86/x86inc: also use GOT for 32-bit cextern calls
    The existing code only does this for cextern_naked, for some reason, even
    though it's also required to make the linker happy when using -fPIC in
    general, not just for external shared libraries.

--- src/x86/x86inc.asm
+++ src/x86/x86inc.asm
@@ -1280,9 +1280,8 @@ INIT_XMM
                 ; the linker will eliminate unnecessary PLT indirections anyway.
                 %define %%j %%i wrt ..plt
             %endif
-        %elif PIC && cglobaled_%2 == 3
-            ; Go through the GOT for functions declared using cextern_naked with
-            ; PIC, as such functions presumably exists in external libraries.
+        %elif PIC && cglobaled_%2 >= 2
+            ; Go through the GOT for functions declared using cextern with PIC
             extern _GLOBAL_OFFSET_TABLE_
             LEA eax, $$+_GLOBAL_OFFSET_TABLE_ wrt ..gotpc
             %define %%j [eax+%%i wrt ..got]
