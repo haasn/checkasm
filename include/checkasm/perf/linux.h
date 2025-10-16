haasn/checkasm

commit 7f415e34884bc87e56ffd878df5b46f730712de8

include/perf.h: rename to CHECKASM_PERF_
Don't collide with possible downstream projects.

include/checkasm/perf/linux.h
@@ -37,22 +37,22 @@
 
 CHECKASM_API int checkasm_get_perf_sysfd(void);
 
-#define PERF_SETUP()\
+#define CHECKASM_PERF_SETUP()\
     int sysfd = checkasm_get_perf_sysfd()
 
-#define PERF_START(t) do {\
-    ioctl(sysfd, PERF_EVENT_IOC_RESET, 0);\
-    ioctl(sysfd, PERF_EVENT_IOC_ENABLE, 0);\
+#define CHECKASM_PERF_START(t) do {\
+    ioctl(sysfd, CHECKASM_PERF_EVENT_IOC_RESET, 0);\
+    ioctl(sysfd, CHECKASM_PERF_EVENT_IOC_ENABLE, 0);\
 } while (0)
 
-#define PERF_STOP(t) do {\
+#define CHECKASM_PERF_STOP(t) do {\
     int _ret;\
-    ioctl(sysfd, PERF_EVENT_IOC_DISABLE, 0);\
+    ioctl(sysfd, CHECKASM_PERF_EVENT_IOC_DISABLE, 0);\
     _ret = read(sysfd, &t, sizeof(t));\
     (void)_ret;\
 } while (0)
 
-#define PERF_NAME "linux (perf)"
-#define PERF_UNIT "tick"
+#define CHECKASM_PERF_NAME "linux (perf)"
+#define CHECKASM_PERF_UNIT "tick"
 
 #endif /* CHECKASM_PERF_LINUX_H */

include/checkasm/perf/aarch64.h
@@ -44,10 +44,10 @@ static inline uint64_t readtime_pmccntr(void) {
     return cycle_counter;
 }
 
-#define PERF_SETUP()
-#define PERF_START(t) t = readtime_pmccntr();
-#define PERF_STOP(t)  t = readtime_pmccntr() - t
-#define PERF_NAME     "aarch64 (pmccntr)"
-#define PERF_UNIT     "cycle"
+#define CHECKASM_PERF_SETUP()
+#define CHECKASM_PERF_START(t) t = readtime_pmccntr();
+#define CHECKASM_PERF_STOP(t)  t = readtime_pmccntr() - t
+#define CHECKASM_PERF_NAME     "aarch64 (pmccntr)"
+#define CHECKASM_PERF_UNIT     "cycle"
 
 #endif /* CHECKASM_PERF_AARCH64_H */

include/checkasm/perf/arm.h
@@ -43,11 +43,11 @@ static inline uint64_t readtime_counter(void) {
     return cycle_counter;
 }
 
-#define PERF_SETUP()
-#define PERF_START(t) t = readtime_counter();
-#define PERF_STOP(t)  t = readtime_counter() - t
-#define PERF_NAME     "arm (ccnt)"
-#define PERF_UNIT     "cycle"
+#define CHECKASM_PERF_SETUP()
+#define CHECKASM_PERF_START(t) t = readtime_counter();
+#define CHECKASM_PERF_STOP(t)  t = readtime_counter() - t
+#define CHECKASM_PERF_NAME     "arm (ccnt)"
+#define CHECKASM_PERF_UNIT     "cycle"
 
 #endif /* !defined(_MSC_VER) && __ARM_ARCH >= 7 */
 #endif /* CHECKASM_PERF_ARM_H */

include/checkasm/perf/dummy.h
@@ -29,10 +29,10 @@
 #ifndef CHECKASM_PERF_DUMMY_H
 #define CHECKASM_PERF_DUMMY_H
 
-#undef PERF_SETUP
-#undef PERF_START
-#undef PERF_STOP
-#define PERF_NAME "none"
-#define PERF_UNIT "n/a"
+#undef CHECKASM_PERF_SETUP
+#undef CHECKASM_PERF_START
+#undef CHECKASM_PERF_STOP
+#define CHECKASM_PERF_NAME "none"
+#define CHECKASM_PERF_UNIT "n/a"
 
 #endif /* CHECKASM_PERF_DUMMY_H */

include/checkasm/perf/gettime.h
@@ -42,10 +42,10 @@ static inline uint64_t clock_gettime_nsec(void) {
     return ((uint64_t)ts.tv_sec*1000000000u) + (uint64_t)ts.tv_nsec;
 }
 
-#define PERF_SETUP()
-#define PERF_START(t) t = clock_gettime_nsec();
-#define PERF_STOP(t)  t = clock_gettime_nsec() - t
-#define PERF_NAME     "clock_gettime"
-#define PERF_UNIT     "nsec"
+#define CHECKASM_PERF_SETUP()
+#define CHECKASM_PERF_START(t) t = clock_gettime_nsec();
+#define CHECKASM_PERF_STOP(t)  t = clock_gettime_nsec() - t
+#define CHECKASM_PERF_NAME     "clock_gettime"
+#define CHECKASM_PERF_UNIT     "nsec"
 
 #endif /* CHECKASM_PERF_GETTIME_H */

include/checkasm/perf/loongarch32.h
@@ -39,10 +39,10 @@ static inline uint64_t readtime_rdtimel(void) {
     return (uint64_t)a;
 }
 
-#define PERF_SETUP()
-#define PERF_START(t) t = readtime_rdtimel();
-#define PERF_STOP(t)  t = readtime_rdtimel() - t
-#define PERF_NAME     "loongarch32 (rdtimel)"
-#define PERF_UNIT     "tick"
+#define CHECKASM_PERF_SETUP()
+#define CHECKASM_PERF_START(t) t = readtime_rdtimel();
+#define CHECKASM_PERF_STOP(t)  t = readtime_rdtimel() - t
+#define CHECKASM_PERF_NAME     "loongarch32 (rdtimel)"
+#define CHECKASM_PERF_UNIT     "tick"
 
 #endif /* CHECKASM_PERF_LOONGARCH32_H */

include/checkasm/perf/loongarch64.h
@@ -39,10 +39,10 @@ static inline uint64_t readtime_rdtime(void) {
     return a;
 }
 
-#define PERF_SETUP()
-#define PERF_START(t) t = readtime_rdtime();
-#define PERF_STOP(t)  t = readtime_rdtime() - t
-#define PERF_NAME     "loongarch64 (rdtime)"
-#define PERF_UNIT     "tick"
+#define CHECKASM_PERF_SETUP()
+#define CHECKASM_PERF_START(t) t = readtime_rdtime();
+#define CHECKASM_PERF_STOP(t)  t = readtime_rdtime() - t
+#define CHECKASM_PERF_NAME     "loongarch64 (rdtime)"
+#define CHECKASM_PERF_UNIT     "tick"
 
 #endif /* CHECKASM_PERF_LOONGARCH64_H */

include/checkasm/perf/mach.h
@@ -31,10 +31,10 @@
 
 #include <mach/mach_time.h>
 
-#define PERF_SETUP()
-#define PERF_START(t) t = mach_absolute_time();
-#define PERF_STOP(t)  t = mach_absolute_time() - t
-#define PERF_NAME     "mach_absolute_time"
-#define PERF_TICK     "tick"
+#define CHECKASM_PERF_SETUP()
+#define CHECKASM_PERF_START(t) t = mach_absolute_time();
+#define CHECKASM_PERF_STOP(t)  t = mach_absolute_time() - t
+#define CHECKASM_PERF_NAME     "mach_absolute_time"
+#define CHECKASM_PERF_TICK     "tick"
 
 #endif /* CHECKASM_PERF_MACH_H */

commit b8d3518ac754ffa1e12bbc85039dbe095d216f25

include/perf/linux.h: fix PERF_ macro names
Incorrectly renamed.

Fixes: 7f415e34884bc87e56ffd878df5b46f730712de8

include/checkasm/perf/linux.h
@@ -41,13 +41,13 @@ CHECKASM_API int checkasm_get_perf_sysfd(void);
     int sysfd = checkasm_get_perf_sysfd()
 
 #define CHECKASM_PERF_START(t) do {\
-    ioctl(sysfd, CHECKASM_PERF_EVENT_IOC_RESET, 0);\
-    ioctl(sysfd, CHECKASM_PERF_EVENT_IOC_ENABLE, 0);\
+    ioctl(sysfd, PERF_EVENT_IOC_RESET, 0);\
+    ioctl(sysfd, PERF_EVENT_IOC_ENABLE, 0);\
 } while (0)
 
 #define CHECKASM_PERF_STOP(t) do {\
     int _ret;\
-    ioctl(sysfd, CHECKASM_PERF_EVENT_IOC_DISABLE, 0);\
+    ioctl(sysfd, PERF_EVENT_IOC_DISABLE, 0);\
     _ret = read(sysfd, &t, sizeof(t));\
     (void)_ret;\
 } while (0)