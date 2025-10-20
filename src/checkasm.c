haasn/checkasm

commit ff0517e3ac7e42cb00ab336cde1582ea84216aff

checkasm: suppress failure messages for tests that should fail

src/checkasm.c
@@ -622,24 +622,26 @@ void *checkasm_check_func(void *const func, const char *const name, ...)
 
 /* Indicate that the current test has failed, return whether verbose printing
  * is requested. */
-#define DEF_FAIL_FUNC(funcname)                                                          \
-    int funcname(const char *const msg, ...)                                             \
-    {                                                                                    \
-        CheckasmFuncVersion *const v = state.current_func_ver;                           \
-        if (v && v->cpu && v->ok) {                                                      \
-            va_list arg;                                                                 \
-                                                                                         \
-            print_cpu_name();                                                            \
-            fprintf(stderr, "   %s_%s (", state.current_func->name, cpu_suffix(v->cpu)); \
-            va_start(arg, msg);                                                          \
-            vfprintf(stderr, msg, arg);                                                  \
-            va_end(arg);                                                                 \
-            fprintf(stderr, ")\n");                                                      \
-                                                                                         \
-            v->ok = 0;                                                                   \
-            state.num_failed++;                                                          \
-        }                                                                                \
-        return cfg.verbose;                                                              \
+#define DEF_FAIL_FUNC(funcname)                                                              \
+    int funcname(const char *const msg, ...)                                                 \
+    {                                                                                        \
+        CheckasmFuncVersion *const v = state.current_func_ver;                               \
+        if (v && v->ok) {                                                                    \
+            va_list arg;                                                                     \
+                                                                                             \
+            if (!state.should_fail) {                                                        \
+                print_cpu_name();                                                            \
+                fprintf(stderr, "   %s_%s (", state.current_func->name, cpu_suffix(v->cpu)); \
+                va_start(arg, msg);                                                          \
+                vfprintf(stderr, msg, arg);                                                  \
+                va_end(arg);                                                                 \
+                fprintf(stderr, ")\n");                                                      \
+            }                                                                                \
+                                                                                             \
+            v->ok = 0;                                                                       \
+            state.num_failed++;                                                              \
+        }                                                                                    \
+        return cfg.verbose && !state.should_fail;                                            \
     }
 
 /* We need to define two versions of this function, one to export and one for

commit 893630cd542574df5a8b8005d6e884653c3dc39a

checkasm: only mark failures for non-C functions
This logic was accidentally changed in a previous commit. Instead of
reverting that change altogether, however, just change the logic to
only print in this case.

Fixes: ff0517e3ac7e42cb00ab336cde1582ea84216aff

src/checkasm.c
@@ -639,7 +639,8 @@ void *checkasm_check_func(void *const func, const char *const name, ...)
             }                                                                                \
                                                                                              \
             v->ok = 0;                                                                       \
-            state.num_failed++;                                                              \
+            if (v->cpu)                                                                      \
+                state.num_failed++;                                                          \
         }                                                                                    \
         return cfg.verbose && !state.should_fail;                                            \
     }