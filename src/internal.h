9de749e9994a0ff78b139f951509df8c34b85024
Niklas Haas <git@haasn.dev>
2025-10-24 11:03:30 +0200

    internal: add mach-based checkasm_gettime_nsec implementation
    clock_gettime(CLOCK_MONOTONIC_RAW) on macOS 10.12+ is just an alias for
    mach_continuous_time(). On older versions, clock_gettime() is not implemented.
    
    This implies that we are safe to simply use mach_absolute_time() (basically
    the same thing as mach_continuous_time for our purposes) on all macOS
    versions.

--- src/internal.h
+++ src/internal.h
@@ -38,6 +38,10 @@
   #include <windows.h>
 #endif
 
+#if defined(__APPLE__) && defined(__MACH__)
+  #include <mach/mach_time.h>
+#endif
+
 #include "checkasm/attributes.h"
 #include "checkasm/test.h"
 
@@ -116,6 +120,13 @@ static inline uint64_t checkasm_gettime_nsec(void)
         QueryPerformanceFrequency(&freq);
     QueryPerformanceCounter(&ts);
     return UINT64_C(1000000000) * ts.QuadPart / freq.QuadPart;
+#elif defined(__APPLE__) && defined(__MACH__)
+    static mach_timebase_info_data_t tb_info;
+    if (!tb_info.denom) {
+        if (mach_timebase_info(&tb_info) != KERN_SUCCESS)
+            return -1;
+    }
+    return mach_absolute_time() * tb_info.numer / tb_info.denom;
 #else
     struct timespec ts;
   #ifdef CLOCK_MONOTONIC_RAW
