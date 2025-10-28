3aab9370ce54c48efaaf1b647004d1dd4e814e35
Niklas Haas <git@haasn.dev>
2025-10-28 22:10:07 +0100

    perf: use target cycle count for NOP overhead loop
    Simplifies the nop measurement function a bit by avoiding an unnecessary
    hard-coded wall-clock time.

--- src/internal.h
+++ src/internal.h
@@ -93,7 +93,7 @@ int checkasm_perf_init(void);
 int checkasm_perf_init_linux(CheckasmPerf *perf);
 int checkasm_perf_init_macos(CheckasmPerf *perf);
 
-CheckasmVar checkasm_measure_nop_cycles(void); /* cycles per iter */
+CheckasmVar checkasm_measure_nop_cycles(uint64_t target_cycles); /* cycles per iter */
 CheckasmVar checkasm_measure_perf_scale(void); /* ns per cycle */
 uint64_t    checkasm_gettime_nsec(void);
 uint64_t    checkasm_gettime_nsec_diff(uint64_t t); /* subtracts t */
