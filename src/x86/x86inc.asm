42a4027ed7c215f59735bc981a9430dd942162b8
Martin Storsjö <martin@martin.st>
2025-10-28 23:12:28 +0200

    x86inc: Do dllexport of the symbols we mark as visible
    Do this if targeting Windows, and if the BUILDING_DLL define is set.

--- src/x86/x86inc.asm
+++ src/x86/x86inc.asm
@@ -818,6 +818,11 @@ BRANCH_INSTR jz, je, jnz, jne, jl, jle, jnl, jnle, jg, jge, jng, jnge, ja, jae, 
     cglobal_internal 1, %1 %+ SUFFIX, %2
 %endmacro
 %macro cvisible 1-2+ "" ; name, [PROLOGUE args]
+%if WIN32
+%ifdef BUILDING_DLL
+    EXPORT mangle(public_prefix %+ _ %+ %1)
+%endif
+%endif
     cglobal_internal 0, %1 %+ SUFFIX, %2
 %endmacro
 %macro cglobal_internal 2-3+
