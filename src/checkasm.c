99c8ae433b66bacb3079d33181e1b20b6f0b17ba
Niklas Haas <git@haasn.dev>
2025-10-29 11:05:06 +0100

    checkasm: keep track of test and report name for each function
    Since we don't know the report name until *after* checkasm_report() is called,
    we need to retroactively keep track of which functions correspond to the same
    group of reports. This is done by linking together functions that were
    initially allocated as part of the same group.
    
    I decided to `strdup`the report name onto every single function, to save
    needing an extra `int` to track which function owns which string.
    
    If C++ browsers can handle millions of allocations per second, C can handle
    a few thousand.

--- src/checkasm.c
+++ src/checkasm.c
@@ -70,8 +70,11 @@ typedef struct CheckasmFuncVersion {
 /* Binary search tree node */
 typedef struct CheckasmFunc {
     struct CheckasmFunc *child[2];
+    struct CheckasmFunc *prev; /* previous function in current report group */
     CheckasmFuncVersion  versions;
     uint8_t              color; /* 0 = red, 1 = black */
+    const char          *test_name;
+    char                *report_name;
     char                 name[];
 } CheckasmFunc;
 
@@ -127,6 +130,7 @@ static void destroy_func_tree(CheckasmFunc *const f)
 
         destroy_func_tree(f->child[0]);
         destroy_func_tree(f->child[1]);
+        free(f->report_name);
         free(f);
     }
 }
@@ -353,6 +357,10 @@ static CheckasmFunc *get_func(CheckasmFunc **const root, const char *const name)
         /* Allocate and insert a new node into the tree */
         const size_t name_length = strlen(name) + 1;
         f = *root = checkasm_malloc(offsetof(CheckasmFunc, name) + name_length);
+        /* Associate this function with each other function that was last used
+         * as part of the same report group */
+        f->prev      = state.current_func;
+        f->test_name = state.current_test_name;
         memcpy(f->name, name, name_length);
     }
 
@@ -771,18 +779,20 @@ void checkasm_report(const char *const name, ...)
 {
     static int    prev_checked, prev_failed;
     static size_t max_length;
+    char          report_name[256];
+
+    va_list arg;
+    va_start(arg, name);
+    vsnprintf(report_name, sizeof(report_name), name, arg);
+    va_end(arg);
 
     const int new_checked = state.num_checked - prev_checked;
     if (new_checked) {
-        int     pad_length = (int) max_length + 4;
-        va_list arg;
+        int pad_length = (int) max_length + 4;
         assert(!state.skip_tests);
 
         print_cpu_name();
-        pad_length -= fprintf(stderr, " - %s.", state.current_test_name);
-        va_start(arg, name);
-        pad_length -= vfprintf(stderr, name, arg);
-        va_end(arg);
+        pad_length -= fprintf(stderr, " - %s.%s", state.current_test_name, report_name);
         fprintf(stderr, "%*c", imax(pad_length, 0) + 2, '[');
 
         if (state.should_fail) {
@@ -807,16 +817,20 @@ void checkasm_report(const char *const name, ...)
     } else if (!state.cpu) {
         /* Calculate the amount of padding required
          * to make the output vertically aligned */
-        size_t  length = strlen(state.current_test_name);
-        va_list arg;
-
-        va_start(arg, name);
-        length += vsnprintf(NULL, 0, name, arg);
-        va_end(arg);
-
+        size_t length = strlen(state.current_test_name) + strlen(report_name);
         if (length > max_length)
             max_length = length;
     }
+
+    /* Store the report name with each function in this report group */
+    CheckasmFunc *func = state.current_func;
+    while (func) {
+        if (!func->report_name)
+            func->report_name = strdup(report_name);
+        func = func->prev;
+    }
+
+    state.current_func = NULL; /* reset current function for new report */
 }
 
 #if ARCH_ARM
