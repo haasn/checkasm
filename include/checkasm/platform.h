f9157acc4ca66e9a76e34f6e09dacb561f87ab22
Martin Storsjö <martin@martin.st>
2025-11-14 22:28:59 +0200

    meson: Use fixed headers instead of having meson install a specific one
    This reduces the reliance on meson logic, making it easier to
    build and use libcheckasm from just a bunch of source files.
    
    The implementation headers are excluded from scanning by doxygen
    though.

---
+++ include/checkasm/platform.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2025, Niklas Haas
+ * Copyright © 2018, VideoLAN and dav1d authors
+ * Copyright © 2018, Two Orioles, LLC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef CHECKASM_PLATFORM_H
+#define CHECKASM_PLATFORM_H
+
+#if defined(__aarch64__) || defined(_M_ARM64) || defined(__arm64ec__)                    \
+    || defined(_M_ARM64EC)
+  #include "checkasm/platform/aarch64.h"
+#elif defined(__arm__) || defined(_M_ARM)
+  #include "checkasm/platform/arm.h"
+#elif defined(__x86_64__) || defined(_M_AMD64)
+  #include "checkasm/platform/x86_64.h"
+#elif defined(__i386__) || defined(_M_IX86)
+  #include "checkasm/platform/x86_32.h"
+#elif defined(__powerpc64__) && defined(__LITTLE_ENDIAN__)
+  #include "checkasm/platform/ppc64le.h"
+#elif defined(__riscv)
+  #include "checkasm/platform/riscv.h"
+#elif defined(__loongarch__)
+  #include "checkasm/platform/loongarch.h"
+#else
+  #include "checkasm/platform/generic.h"
+#endif
+
+#endif /* CHECKASM_PLATFORM_H */
