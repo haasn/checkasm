haasn/checkasm

commit aebbc52b92dfda5598509a0d9d7735ef7e0730ef

checkasm: add --repeat option
Can be used to automatically repeat the test, either a fixed number of
iterations has passed, or until all seeds have been exhaustively tested.

Closes: #12

src/checkasm.c
@@ -807,6 +807,8 @@ int checkasm_run(const CheckasmConfig *config)
 
     if (!cfg.seed)
         cfg.seed = (unsigned) checkasm_gettime_nsec();
+    if (!cfg.repeat)
+        cfg.repeat = 1;
     if (!cfg.bench_usec)
         cfg.bench_usec = 1000;
 
@@ -846,16 +848,29 @@ int checkasm_run(const CheckasmConfig *config)
 
     print_info();
 
-    check_cpu_flag(NULL);
-    for (int i = 0; i < cfg.nb_cpu_flags; i++)
-        check_cpu_flag(&cfg.cpu_flags[i]);
+    for (unsigned i = 0; i < cfg.repeat; i++) {
+        if (i > 0) {
+            checkasm_fprintf(stderr, COLOR_YELLOW, "\nTest #%d:\n", i + 1);
+            fprintf(stderr, " - Random seed: %u\n", cfg.seed);
+        }
+
+        check_cpu_flag(NULL);
+        for (int i = 0; i < cfg.nb_cpu_flags; i++)
+            check_cpu_flag(&cfg.cpu_flags[i]);
+
+        destroy_func_tree(state.current.funcs);
+        memset(&state.current, 0, sizeof(state.current));
+        cfg.seed++;
+
+        if (state.num_failed || state.num_skipped)
+            break;
+    }
 
     print_summary();
 
     if (state.num_benched && !state.num_failed)
         print_benchmarks();
 
-    destroy_func_tree(state.current.funcs);
     return state.num_failed || state.num_skipped;
 }
 
@@ -1036,6 +1051,7 @@ static void print_usage(const char *const progname)
             "    --list-tests               List available tests\n"
             "    --duration=<μs>            Benchmark duration (per function) in "
             "μs\n"
+            "    --repeat[=<N>]             Repeat tests N times, on successive seeds\n"
             "    --test=<pattern> -t        Test only <pattern>\n"
             "    --verbose -v               Print verbose timing info and failure "
             "data\n",
@@ -1113,6 +1129,15 @@ int checkasm_main(CheckasmConfig *config, int argc, const char *argv[])
                 print_usage(argv[0]);
                 return 1;
             }
+        } else if (!strncmp(argv[1], "--repeat=", 9)) {
+            const char *const s = argv[1] + 9;
+            if (!parseu(&config->repeat, s, 10)) {
+                fprintf(stderr, "checkasm: invalid number of repetitions (%s)\n", s);
+                print_usage(argv[0]);
+                return 1;
+            }
+        } else if (!strcmp(argv[1], "--repeat")) {
+            config->repeat = UINT_MAX;
         } else {
             if (!parseu(&config->seed, argv[1], 10)) {
                 fprintf(stderr, "checkasm: unknown option (%s)\n", argv[1]);

include/checkasm/checkasm.h
@@ -92,6 +92,10 @@ typedef struct CheckasmConfig {
     /* If nonzero, use the specified seed for random number generation. */
     unsigned seed;
 
+    /* Repeat the test (and benchmark, if enabled) this many times, on
+     * successive seeds. Setting -1 effectively tests every possible seed. */
+    unsigned repeat;
+
     /* If nonzero, the process will be pinned to the specified CPU (id) */
     unsigned cpu_affinity;
 } CheckasmConfig;

commit f4e70d984db138291a56ec2a5f3e8b9cbbcbb2e3

checkasm: print summary after each test run
When using --repeat, we should print the summary (log of completed tests)
after each test run; before destroying the function tree - otherwise, we
never actually print any benchmark results.

This is not exactly the state I want to end up in - ideally, we should
keep aggregating the benchmark data across all repetitions. However, this
is difficult to do with the current architecture and unlikely to be of much
use as one would normally rarely combine --repeat and --bench; instead you
would increase --duration.

Though for completeness' sake I would like to note that allowing --repeat
to aggregate benchmark results would allow testing functions throughout
distinct points in time, which would provide much greater resilience against
uncontrollable external factors like CPU scaling. Long term, this will be
possible once I re-engineer stuff to let individual tests run on separate
threads (with separate function trees) and merge them in the end.

Fixes: aebbc52b92dfda5598509a0d9d7735ef7e0730ef

src/checkasm.c
@@ -880,15 +880,16 @@ int checkasm_run(const CheckasmConfig *config)
         for (int i = 0; i < cfg.nb_cpu_flags; i++)
             check_cpu_flag(&cfg.cpu_flags[i]);
 
+        int res = print_summary();
         destroy_func_tree(state.current.funcs);
+        if (res)
+            return res;
+
         memset(&state.current, 0, sizeof(state.current));
         cfg.seed++;
-
-        if (state.num_failed || state.num_skipped)
-            break;
     }
 
-    return print_summary();
+    return 0;
 }
 
 /* Decide whether or not the specified function needs to be tested and