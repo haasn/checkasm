355969db733c04e27e88e85ebfe4ccfc4a841156
Rémi Denis-Courmont <remi@remlab.net>
2025-12-20 12:01:20 +0200

    riscv: best effort to clobber argument registers
    To support forward-edge CFI, the prototype of the function (pointer)
    used to check calls must be the exact same as that of the actual
    function being checked. As such, it will no longer be possible to rely
    on the checked_call long argument list to clobber unused argument
    registers.
    
    This adds a best effort ABI-compliant method to clobber registers as
    late as possible, i.e. right before the checked all. In practice, it
    works because the compiler has no reasons to set unused argument
    registers at that point.

--- include/checkasm/platform/riscv.h
+++ include/checkasm/platform/riscv.h
@@ -39,12 +39,14 @@
 
 #define checkasm_call_checked(func, ...)                                                 \
     (checkasm_set_signal_handler_state(1),                                               \
+     checkasm_clobber_args(),                                                            \
      checked_call(func, 0, 0, 0, 0, 0, 0, 0, __VA_ARGS__, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0,  \
                   0, 0, 0, 0, 0, 0));                                                    \
     checkasm_set_signal_handler_state(0)
 
 #define CHECKASM_ALIGNMENT 16
 
+void checkasm_clobber_args(void);
 int checkasm_has_float(void);
 int checkasm_has_vector(void);
 
