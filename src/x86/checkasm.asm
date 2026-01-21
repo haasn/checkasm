haasn/checkasm

commit 8363606848454a64b04632e05c8d93eb0b5570b5

x86: don't assume checkasm_fail_abort() returns
This is marked noreturn, so we don't need to restore anything after jumping to
it.

src/x86/checkasm.asm
@@ -215,13 +215,8 @@ cvisible empty_mmx, 0, 0
 %endmacro
 
 %macro REPORT_FAILURE 1 ; err_msg
-    mov           r10, rax
-    mov           r11, rdx
     lea            r0, [%1]
-    xor           eax, eax
-    call fail_abort
-    mov           rax, r10
-    mov           rdx, r11
+    jmp fail_abort
 %endmacro
 
 %macro checked_call_fn 0-1
@@ -432,13 +427,9 @@ WARMUP
 %assign n6 0x33627ba7
 
 %macro REPORT_FAILURE 1 ; err_msg
-    mov            r5, eax
-    mov            r6, edx
     LEA            r1, %1
     mov         [esp], r1
-    call fail_abort
-    mov           eax, r5
-    mov           edx, r6
+    jmp fail_abort
 %endmacro
 
 ;-----------------------------------------------------------------------------

commit c727b4b281d3e2ee7c553405804cf0b8070d1a51

x86: Fix checkasm_fail_abort() call
We need to clear `al` to 0 for variadic function calls which do not
pass any floating point args, as per the System V x86_64 ABI.

(On other platforms, this is harmless/irrelevant)

Fixes: 8363606848454a64b04632e05c8d93eb0b5570b5

src/x86/checkasm.asm
@@ -216,6 +216,7 @@ cvisible empty_mmx, 0, 0
 
 %macro REPORT_FAILURE 1 ; err_msg
     lea            r0, [%1]
+    xor           eax, eax
     jmp fail_abort
 %endmacro
 