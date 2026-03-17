9a9c63d69386e1cf09424fa127f23b6c6536d947
Martin Storsjö <martin@martin.st>
2026-03-17 15:48:53 +0200

    meson: Don't globally define _GNU_SOURCE
    If embedded in a foreign build system, they may be building checkasm
    without _GNU_SOURCE.
    
    When building on e.g. Linux, we do need _GNU_SOURCE defined for
    e.g. strdup() to be exposed from string.h, but other projects may
    achieve this through other means (e.g. defining _XOPEN_SOURCE or
    _POSIX_C_SOURCE), while those other means might not enable
    the syscall function to be visible.
    
    To ease compatibility with such projects, don't define _GNU_SOURCE
    globally, but just define _XOPEN_SOURCE and _POSIX_C_SOURCE (which
    matches what ffmpeg does).
    
    For testing for pthread_setaffinity_np, we need to explicitly define
    _GNU_SOURCE during that test though.

--- src/perf/linux.c
+++ src/perf/linux.c
@@ -30,9 +30,6 @@
 
 #if HAVE_LINUX_PERF
 
-  /* We define this globally in Meson, but foreign build systems might not
-   * define exactly this define, so make sure it is enabled. This is required
-   * for the syscall function to be visible. */
   #ifndef _GNU_SOURCE
     #define _GNU_SOURCE
   #endif
